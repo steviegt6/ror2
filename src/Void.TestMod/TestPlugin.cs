53d2dee1200cf05a52b23155b8613f0410a79c4f
Tomat <xxlennygamerxx@gmail.com>
2023-03-26 00:45:04 -0700

    Wacky weird debugging...

---
+++ src/Void.TestMod/TestPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BepInEx;
+
+namespace Void.TestMod; 
+
+[BepInPlugin(GUID, NAME, VERSION)]
+public class TestPlugin : BaseUnityPlugin {
+    public const string GUID = "Tomat.Void.TestMod";
+    public const string NAME = "TestMod";
+    public const string VERSION = "1.0.0";
+
+    private void Awake() {
+        Logger.LogInfo("Hello, world!");
+    }
+}
